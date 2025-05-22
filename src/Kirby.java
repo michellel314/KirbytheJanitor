2137bac94f2e42e1ae4ffd67906505f45107ac5f
michellel314 <michelle.lee.087@gmail.com>
2025-05-22 13:49:21 -0400

    First Commmit - Beginning to program frame and Kirby to appear/move

--- src/Kirby.java
+++ src/Kirby.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Kirby {
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class Kirby {
+    private int x;
+    private int y;
+    private int speed = 4;
+
+    private int score;
+    private BufferedImage[] walkFrames;
+    private int currentFrame;
+    private int frameCounter;
+    private Vacuum vacuum;
+
+    public Kirby(int x, int y){
+        this.x = x;
+        this.y = y;
+        vacuum = new Vacuum (1);
+        score = 0;
+    }
+
+    public void move(int dx, int dy){
+        if(dx != 0 || dy != 0){
+            x += dx * speed;
+            y += dy * speed;
+
+            frameCounter++;
+            if(frameCounter >= 10){
+                currentFrame = (currentFrame + 1) % walkFrames.length;
+            }
+        }
+    }
 }
--- src/Window.java
+++ src/Window.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Window {
+import javax.swing.JFrame;
+public class Window {
+    public Window(){
+        JFrame frame = new JFrame("f");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+    }
 }
